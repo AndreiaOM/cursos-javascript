aa9e82d3595b45f833c63f19bb2c348fe1af023b
AndreiaOM <andreiaolimacedo@gmail.com>
2022-03-30 00:06:56 -0300

    JavaScript ES6 essencial.

---
+++ javascriptES6-essencial/Aula3.js
@@ -0,0 +1,7 @@
+//Herança (baseada em prototipos, prototype, _proto_, constructor)
+
+__proto__ -> prototype -> constructor
+
+const myText = 'Hello prototype!';
+myText.constructor -> String
+myText.__proto__ -> String.prototype
